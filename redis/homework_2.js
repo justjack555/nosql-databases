2ce791e204813b6d13e5bdb09140514b11866b75
Jack <jjricci555@gmail.com>
2018-04-09 10:10:27 -0400

    [HW 2] Cleaned up logging for submission

--- redis/homework_2.js
+++ redis/homework_2.js
@@ -5,18 +5,10 @@ var ONE_WEEK_IN_MILLISECONDS = 1000 * ONE_WEEK_IN_SECONDS;
 var VOTE_SCORE = 432;
 
 function article_vote(client, user, article) {
-    var cutoff = Date.now() - ONE_WEEK_IN_MILLISECONDS;
-    //var article_time = 1000 * client.zscore("time:", article);
-   // client.zscore('time', article, redis.print);
-//    if (! article_time < cutoff) {
-        var article_id = article.split(':')[1];
-//       console.log("In article vote...");
+     var article_id = article.split(':')[1];
 	 client.sadd('voted:' + article_id, user)
-            client.zincrby('score:', VOTE_SCORE, article);
-            client.hincrby(article, 'votes', 1);
-//console.log("Leaving article vote...");
-        
-//    }
+     client.zincrby('score:', VOTE_SCORE, article);
+     client.hincrby(article, 'votes', 1);
 }
 
 function article_switch_vote(client, user, from_article, to_article) {
@@ -25,93 +17,47 @@ function article_switch_vote(client, user, from_article, to_article) {
     var from_article_id = from_article.split(':')[1];
     var to_article_id = to_article.split(':')[1];
 
-    // LOGGING: Each important piece of info
-/*    console.log("Originally: ");
-    console.log("From_article_id: " + from_article_id);
-    console.log("User is in from article set: " + client.sismember('voted:' + from_article_id, user, redis.print));
-    console.log("User is in to_article set: " + client.sismember('voted:' + to_article_id, user, redis.print));
-    console.log("from_article votes: " + client.hget(from_article, 'votes'));
-    console.log("to_articel votes: " + client.hget(to_article, 'votes'));
-*/
     // First remove vote from other article set
     client.srem('voted:' + from_article_id, user);
 
     // Decrement score
     client.zincrby('score:', -VOTE_SCORE, from_article);
     client.hincrby(from_article, 'votes', -1);
-    // LOGGING: Each important piece of info
-/*    console.log("After remove: ");
-    console.log("User is in from article set: " + client.sismember('voted:' + from_article_id, user));
-    console.log("User is in to_article set: " + client.sismember('voted:' + to_article_id, user));
-    console.log("from_article votes: " + client.hget(from_article, 'votes'));
-    console.log("to_articel votes: " + client.hget(to_article, 'votes'));
-*/
 
-    // Then same code as switching vote to another article
-       client.sadd('voted:' + to_article_id, user);
-        client.zincrby('score:', VOTE_SCORE,to_article);
-        client.hincrby(to_article, 'votes', 1);
-
-    // LOGGING: Each important piece of info
-/*    console.log("After add: ");
-    console.log("User is in from article set: " + client.sismember('voted:' + from_article_id, user));
-    console.log("User is in to_article set: " + client.sismember('voted:' + to_article_id, user));
-    console.log("from_article votes: " + client.hget(from_article, 'votes'));
-    console.log("to_articel votes: " + client.hget(to_article, 'votes'));
-*/
-
+    // Then same code as adding vote to another article
+    client.sadd('voted:' + to_article_id, user);
+    client.zincrby('score:', VOTE_SCORE, to_article);
+    client.hincrby(to_article, 'votes', 1);
 }
 
+//Create client
 var client = redis.createClient();
+
+// Wait for connection to be made
 client.on('connect', function(){
-	console.log('connected...');
-article_vote(client, "user:3", "article:1");
+	article_vote(client, "user:3", "article:1");
 
-// user:3 up votes article:3
-article_vote(client, "user:3", "article:3")
-//console.log(client.hget("article:3", "title"));
-// user:2 switches their vote from article:8 to article:1
-article_switch_vote(client, "user:2", "article:8", "article:1")
+	// user:3 up votes article:3
+	article_vote(client, "user:3", "article:3")
 
-// Which article's score is between 10 and 20?
-// PRINT THE ARTICLE'S LINK TO STDOUT:
-// HOMEWORK 2 Part II
-client.zrangebyscore('score:', 10, 20, function(err, reply){
-	console.log("sorted set score results are: " + reply[0]);
-	console.log("Length of set is: " + reply.length);
-	client.hget(reply[0], 'link', function(err, link){
-		console.log("Link is: " + link);
-		client.quit();
+	// user:2 switches their vote from article:8 to article:1
+	article_switch_vote(client, "user:2", "article:8", "article:1")
+
+	// Which article's score is between 10 and 20?
+	// PRINT THE ARTICLE'S LINK TO STDOUT:
+	// HOMEWORK 2 Part II
+	client.zrangebyscore('score:', 10, 20, function(err, reply){
+		//We only should have a single result
+		if(reply.length != 1){
+			console.log("Unexpected number of results in specified score range. Check data load");
+		}
+	
+		// Use single result in set to obtain value in link field
+		client.hget(reply[0], 'link', function(err, link){
+			console.log(link);
+	
+			// Kill client before program termination
+			client.quit();
+		});	
 	});
 });
-
-//client.quit();
-});
-/*
-// user:3 up votes article:1
-article_vote(client, "user:3", "article:1")
-// user:3 up votes article:3
-article_vote(client, "user:3", "article:3")
-console.log(client.hget("article:3", "title"));
-// user:2 switches their vote from article:8 to article:1
-article_switch_vote(client, "user:2", "article:8", "article:1")
-
-// Which article's score is between 10 and 20?
-// PRINT THE ARTICLE'S LINK TO STDOUT:
-// HOMEWORK 2 Part II
-var article = client.zrangebyscore('score:', 10, 20);
-console.log(client.hget(article, 'link'));
-*/
-//clirticle_vote(client, "user:3", "article:1")
-// user:3 up votes article:3
-/*
-article_vote(client, "user:3", "article:3")
-console.log(client.hget("article:3", "title"));
-// user:2 switches their vote from article:8 to article:1
-article_switch_vote(client, "user:2", "article:8", "article:1")
-
-// Which article's score is between 10 and 20?
-// PRINT THE ARTICLE'S LINK TO STDOUT:
-// HOMEWORK 2 Part II
-var article = client.zrangebyscore('score:', 10, 20);
-console.log(client.hget(article, 'link'));ent.quit();*/
