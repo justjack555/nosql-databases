4b3349a488c7548cdde9cd4386140ff562eddcb5
Jack <jjricci555@gmail.com>
2018-05-06 21:11:49 -0400

    [Final] Consolidated data loading into final_project file and finished written part

--- mongodb/final_project.js
+++ mongodb/final_project.js
@@ -1,37 +1,49 @@
 // Put the use case you chose here. Then justify your database choice:
-//
+//  - I selected the photo sharing site as use-case. I selected MongoDB because
+// documents with nested field values served as the ideal way to represent the
+// application's back-end. MongoDB allowed me to divide the data into 2 natural
+// collections : users and media, one of which held user data, the other holding
+// photo/video specific data. The two collections hold references to data in the
+// other by storing id values (the unique identifier of Mongo documents) that
+// represent documents in the other collection.
 //
 // Explain what will happen if coffee is spilled on one of the servers in your cluster, causing it to go down.
+// - We would use replication to ensure that when that server goes down, we still have other
+// servers that hold copies of the data on that machine so that content can still be served
+// as thought server A never went down.
 //
 //
 // What data is it not ok to lose in your app? What can you do in your commands to mitigate the risk of lost data?
+// - Cannot lose user_ids or media_ids, as much of the cross referencing between collections
+// is done by using these ids. We can use write concerns and replication in the insert/update
+// commands in order to ensure that all data written to the data based is safely written to
+// multiple storage locations before we proceed on with the code.
 //
-//
 
 
 
-//// Action 1: <describe the action here>
+//// Action 1: The user named "Dan House" signs up for a Jackstagram account
 
 
-// Action 2: <describe the action here>
+// Action 2: "Dan House" follows the user "Jack Ricci"
 
 
-// Action 3: <describe the action here>
+// Action 3: "Dan House" comments on one of the user "Jack Ricci"'s photo
 
 
-// Action 4: <describe the action here>
+// Action 4: "Dan House" uploads his first photo to Jackstagram
 
 
-// Action 5: <describe the action here>
+// Action 5: "Dan House" by accident likes his own photo via a heart reaction
 
 
-// Action 6: <describe the action here>
+// Action 6: "Dan House" views the last 3 photos uploaded (by date) by people he follows
 
 
-// Action 7: <describe the action here>
+// Action 7: "Dan House" views all of the user "Jack Ricci"'s photos
 
 
-// Action 8: <describe the action here>
+// Action 8: "Dan House" unfollows "Jack Ricci"
 
 
 
@@ -58,6 +70,93 @@ const photoWeight = 300000;
 const minVidSize = 1000000;
 const vidWeight = 100000000;
 
+// Our three users
+const usersData = [{
+    _id: 1,
+    name : "Jack Ricci",
+    followers : [2],
+    following : [2, 3],
+    activity: [ {
+        media_id: 1,
+        type: "reaction",
+        time: new Date('May 1, 2018 03:24:00'),
+        reaction: {
+            poster: 1,
+            recipient: 2,
+            type: "heart"
+        }
+    }, {
+        media_id: 2,
+        type: "reaction",
+        time: new Date('April 30, 2018 03:24:00'),
+        reaction: {
+            poster: 1,
+            recipient: 2,
+            type: "like"
+        }
+    }, {
+        media_id: 3,
+        type: "comment",
+        time: new Date('May 5, 2018 12:00:00'),
+        comment: {
+            poster: 1,
+            recipient: 1,
+            value: "Love this pic."
+        }
+    }],
+    posted_media: [3, 5, 7, 9, 11, 13]
+}, {
+    _id: 2,
+    name : "Ben Stephens",
+    followers : [1, 3],
+    following : [1],
+    activity: [ {
+        media_id: 1,
+        type: "comment",
+        time: new Date('May 1, 2018 03:30:00'),
+        comment: {
+            poster: 2,
+            recipient: 2,
+            value: "Thanks for all the love!"
+        }
+    }, {
+        media_id: 3,
+        type: "reaction",
+        time: new Date('August 30, 2018 03:24:00'),
+        reaction: {
+            poster: 2,
+            recipient: 1,
+            type: "like"
+        }
+    }],
+    posted_media: [1, 2, 4, 6, 8, 10, 12]
+}, {
+    _id: 3,
+    name : "Greg Smith",
+    followers : [1],
+    following : [2],
+    activity: [ {
+        media_id: 1,
+        type: "reaction",
+        time: new Date('May 1, 2018 03:25:00'),
+        reaction: {
+            poster: 3,
+            recipient: 2,
+            type: "like"
+        }
+    }, {
+        media_id: 2,
+        type: "comment",
+        time: new Date('May 30, 2018 03:24:00'),
+        comment: {
+            poster: 3,
+            recipient: 2,
+            value: "You're my hero"
+        }
+    }],
+    posted_media: [14, 15]
+}];
+
 // Base object for new user
 const newUser = {
     name : "Dan House",
@@ -73,6 +172,146 @@ const newUser = {
 function closeConn(client){
     console.log("\nClosing connection with server...");
     client.close();
+}
+
+/**
+ * Function to insert pandas short
+ */
+function addUsers(db, callback){
+    console.log("Adding users...");
+
+    const collection = db.collection(usersColl);
+
+    // Simple insertion of panda doc
+    collection.insertMany(usersData, function(err, result){
+        assert.equal(err, null);
+        console.log("Insertion of " + result.result.n + " users successful...");
+        callback();
+    });
+}
+
+/**
+ * Simple function to pick media type
+ */
+function genMediaType(mediaEntry){
+    mediaEntry.type = Math.floor(2*Math.random()) === 1 ? photoType : videoType;
+    return mediaEntry.type;
+}
+
+/**
+ * Fill data field based upon type of media
+ * @param mediaEntry
+ */
+function fillMediaData(mediaEntry){
+    var dataObject = {};
+
+    // Fill in data field with photo or video object
+    if(mediaEntry.type === photoType){
+        dataObject.file_size = Math.floor(photoWeight*Math.random()) + minPhotoSize;
+        dataObject.filter = "Valencia";
+        mediaEntry.photoData = dataObject;
+    }else{
+        dataObject.file_size = Math.floor(vidWeight*Math.random()) + minVidSize;
+        dataObject.length = dataObject.file_size/minVidSize;
+        mediaEntry.videoData = dataObject;
+    }
+
+    return 1;
+}
+
+/**
+ * Load comments and reactions into media objects
+ */
+function loadReactions(mediaEntry){
+    var i;
+    mediaEntry.reactions = [];
+    mediaEntry.comments = [];
+    mediaEntry.stats = {
+        num_reax : 0,
+        num_comments : 0
+    };
+
+    // Iterate through each user
+    for(i = 0; i < usersData.length; i++){
+        const user = usersData[i];
+        // Iterate through user's activity
+        var j;
+        for(j = 0; j < user.activity.length; j++){
+            const action = user.activity[j];
+            if(action.media_id === mediaEntry._id){
+                if(action.type === "reaction"){
+                    mediaEntry.reactions.push(action.reaction);
+                }else{
+                    mediaEntry.comments.push(action.comment);
+                }
+            }
+        }
+    }
+
+    mediaEntry.stats.num_reax = mediaEntry.reactions.length;
+    mediaEntry.stats.num_comments = mediaEntry.comments.length;
+    return 1;
+}
+
+/**
+ * Add media using user info
+ */
+function addMedia(db, callback){
+    var mediaData = [];
+
+    const collection = db.collection(mediaColl);
+    console.log("Adding media objects...");
+
+    // Iterate through users objects
+    for(var i = 0; i < usersData.length; i++){
+        const user = usersData[i];
+        if(!user.hasOwnProperty("posted_media")){
+            console.log("ADD_MEDIA: Error - no posted_media property for user. Exiting");
+            callback();
+            return;
+        }
+
+        // Craft media object
+        for(var j = 0; j < user.posted_media.length; j++){
+            const postId = user.posted_media[j];
+            var mediaEntry = {};
+
+            mediaEntry._id = postId;
+            // Type of media
+            if(genMediaType(mediaEntry) === null){
+                console.log("ADD_MEDIA: Error - unable to initialize type property. Exiting.");
+                callback();
+                return;
+            }
+
+            // Video/photo statistics
+            if(fillMediaData(mediaEntry) === null){
+                console.log("ADD_MEDIA: Error - unable to initialize data property. Exiting.");
+                callback();
+                return;
+            }
+
+            // Add date
+            const day = Math.floor(31*Math.random()) + 1;
+            mediaEntry.time_posted = new Date('March ' + day + ', 2018 03:24:00');
+
+            // Add reactions and comments
+            if(loadReactions(mediaEntry) === null){
+                console.log("ADD_MEDIA: Error - unable to load reactions. Exiting.");
+                callback();
+                return;
+            }
+
+            mediaData.push(mediaEntry);
+        }
+    }
+
+    // Simple insertion of panda doc
+    collection.insertMany(mediaData, function(err, result){
+        assert.equal(err, null);
+        console.log("Insertion of " + result.result.n + " media entries successful...");
+        callback();
+    });
 }
 
 /**
@@ -374,40 +613,45 @@ MongoClient.connect(url, function(err, client) {
     const db = client.db(dbName);
     const followingName = "Jack Ricci";
 
-    //Sign up for account
-    console.log("\nSigning up for account...");
-    addUser(db, function(newId){
-        // Follow someone new
-        console.log("\nUser " + newId + " attempting to follow someone new...");
-        followJack(db, newId, followingName, function(){
+    // Insert users
+    console.log("\nLoading all preliminary data...");
+    addUsers(db, function(){
+        addMedia(db, function(){
+            //Sign up for account
+            console.log("\nSigning up for account...");
+            addUser(db, function(newId){
+                // Follow someone new
+                console.log("\nUser " + newId + " attempting to follow someone new...");
+                followJack(db, newId, followingName, function(){
 
-            // Comment on a post
-            console.log("\nUser " + newId + " attempting to add a comment...");
-            addComment(db, newId, followingName, function(){
-                // Upload a photo
-                console.log("\nUploading first media object...");
-                uploadMedia(db, newId, function(){
-                    // Like your own photo by accident
-                    console.log("\nLiking own photo by accident...");
-                    ownLike(db, newId, function(){
-                        // Find two most recent posts
-                        console.log("\nFinding three most recent posts in feed...");
-                        lastThree(db, newId, function(){
-                            // See all of the photos from one other user
-                            console.log("\nFinding all of Jack Riccis photos...");
-                            oneUser(db, newId, function(){
-                                // Unfollow other user
-                                console.log("\nUnfollowing Jack Ricci");
-                                unfollow(db, newId, followingName, function(){
-                                    // Close connection
-                                    closeConn(client);
+                    // Comment on a post
+                    console.log("\nUser " + newId + " attempting to add a comment...");
+                    addComment(db, newId, followingName, function(){
+                        // Upload a photo
+                        console.log("\nUploading first media object...");
+                        uploadMedia(db, newId, function(){
+                            // Like your own photo by accident
+                            console.log("\nLiking own photo by accident...");
+                            ownLike(db, newId, function(){
+                                // Find two most recent posts
+                                console.log("\nFinding three most recent posts in feed...");
+                                lastThree(db, newId, function(){
+                                    // See all of the photos from one other user
+                                    console.log("\nFinding all of Jack Riccis photos...");
+                                    oneUser(db, newId, function(){
+                                        // Unfollow other user
+                                        console.log("\nUnfollowing Jack Ricci");
+                                        unfollow(db, newId, followingName, function(){
+                                            // Close connection
+                                            closeConn(client);
+                                        });
+                                    });
                                 });
                             });
                         });
                     });
                 });
             });
-
         });
     });
 });
