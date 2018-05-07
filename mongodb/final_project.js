ba2d5950b9d9c4e9582895b723715e355abb4276
Jack <jjricci555@gmail.com>
2018-05-06 20:22:33 -0400

    [Final] Fixed logging and improved comments

--- mongodb/final_project.js
+++ mongodb/final_project.js
@@ -71,174 +71,9 @@ const newUser = {
  * Function to be invoked at connection closing time
  */
 function closeConn(client){
-    console.log("Closing connection with server...");
+    console.log("\nClosing connection with server...");
     client.close();
 }
-
-/**
- * Function to update unrated shorts
- */
-/*
-function updateUnrated(db, callback){
-    const collection = db.collection(collName);
-
-    // Genre must be short, rating must be UNRATED
-    const unratedParam = {
-        "genres" : "Short",
-        "rated" : "NOT RATED"
-    };
-
-    const unratedUpdate = { $set: { rated : "Pending rating" } };
-
-    collection.updateMany(unratedParam, unratedUpdate, function(err, result) {
-        assert.equal(err, null);
-        console.log("Successfully updated rated field for " + result.modifiedCount + " documents...");
-        callback();
-    });
-}
-*/
-
-/**
- * Function to insert pandas short
- */
-/*
-function insertPandas(db, callback){
-//    console.log("Inserting pandas short...");
-
-    const collection = db.collection(collName);
-
-    // Simple insertion of panda doc
-    collection.insertOne(pandasDoc, function(err, result){
-        assert.equal(err, null);
-        console.log("Insertion of " + result.result.n + " Pandas movie document successful...");
-        callback();
-    });
-}
-*/
-
-/**
- * Function that prints the number of shorts in the movies
- * collection
- */
-/*function countShorts(db, callback){
-    const collection = db.collection(collName);
-
-    const pipeline = [ {
-        $match: {
-            genres: "Short"
-        }},
-        {
-            $group: {
-                _id: "Short",
-                count: {
-                    $sum: 1
-                }
-            }
-        }];
-
-    collection.aggregate(pipeline, function(err, cursor){
-        cursor.toArray(function(error, documents){
-            assert.equal(error, null);
-            documents.forEach(function(doc){
-                console.log(doc);
-            });
-
-            callback();
-        });
-    });
-}
-*/
-
-/**
- * Function to count US movies
- */
-/*function countUSMovies(db, callback){
-    const collection = db.collection(collName);
-
-    const pipeline = [ {
-        $match: {
-            countries: "USA",
-            rated : "Pending rating"
-        }},
-        {
-            $group: {
-                _id: {
-                    country: "USA",
-                    rating : "Pending rating"
-                },
-                count: {
-                    $sum: 1
-                }
-            }
-        }];
-
-    collection.aggregate(pipeline, function(err, cursor){
-        cursor.toArray(function(error, documents){
-            assert.equal(error, null);
-            documents.forEach(function(doc){
-                console.log(doc);
-            });
-
-            callback();
-        });
-    });
-}
-*/
-/*
-function addVotes(db, callback){
-    const joinColl = db.collection("test");
-
-    joinColl.insertMany([{
-        title: "Pandas",
-        vote: "bad"
-    }, {
-        title: "Pandas",
-        vote: "good"
-    }, {
-        title: "Pandas",
-        vote: "bad"
-    }, {
-        title: "Very Private Lesson",
-        vote: "good"
-    }], function(err, reply){
-        assert.equal(err, null);
-        callback();
-    });
-
-
-}
-*/
-/*
-function joinOnVotes(db, callback){
-    const collection = db.collection(collName);
-    const collToJoin = "test";
-
-    const pipeline = [{
-        $lookup: {
-            from: collToJoin,
-            localField: "title",
-            foreignField: "title",
-            as: "votes"
-        }
-    }];
-
-    console.log("Computing lookup results...");
-    collection.aggregate(pipeline, function(err, cursor){
-        cursor.toArray(function(error, documents){
-            assert.equal(error, null);
-
-            documents.forEach(function(doc){
-                if(doc.votes.length > 0){
-                    console.log("The following movie has votes in the test collection: ");
-                    console.log(doc);
-                }
-            });
-
-            callback();
-        });
-    });
-}
-*/
 
 /**
  * Function to insert new user
@@ -264,12 +99,12 @@ function followJack(db, newId, followingName, callback){
     const collection = db.collection(usersColl);
 
     // Find user to follow and update his/her followers list
-    collection.findOneAndUpdate({name : "Jack Ricci"},
+    collection.findOneAndUpdate({name : followingName},
         {$push : {followers : newId}},
         {returnOriginal : false},
         function(err, result){
             assert.equal(err, null);
-            console.log("FOLLOW_JACK: New length of Jack's follow list is: " + result.value.followers.length);
+            console.log("New length of Jack's followers list is: " + result.value.followers.length);
 
             // Modify new user's following
             collection.findOneAndUpdate({_id: newId},
@@ -277,7 +112,7 @@ function followJack(db, newId, followingName, callback){
                 {returnOriginal : false},
                 function(err, result){
                     assert.equal(err, null);
-                    console.log("FOLLOW_JACK: New length of Dan's following list is: " + result.value.following.length);
+                    console.log("New length of Dan's following list is: " + result.value.following.length);
                     callback();
             });
     });
@@ -303,13 +138,12 @@ function addComment(db, newid, following, callback){
             assert.equal(err, null);
             // Get last post
             const lastPost = result.posted_media[result.posted_media.length - 1];
-            console.log("ADD_COMMENT: Last post for user " + result._id + "had media_id " + lastPost);
             const newComment = {
                 poster: newid,
                 recipient: result._id,
                 value: "So excited to now be following you!!"
             };
-            console.log("ADD_COMMENT: New comment by poster: " + newComment.poster + " to recipient " + newComment.recipient);
+            console.log("New comment by poster: " + newComment.poster + " to recipient " + newComment.recipient + " added successfully...");
 
             // Find and update the media in the media collection corresponding to this image
             mediaCollection.findOneAndUpdate({_id : lastPost},
@@ -335,7 +169,6 @@ function addComment(db, newid, following, callback){
                             activity : newActivity
                         }}, function(err, result){
                                 assert.equal(err, null);
-                                console.log("Successfully updated " + result.value.name + "'s activity...");
                                 callback();
                         });
                 });
@@ -366,14 +199,12 @@ function uploadMedia(db, newId, callback){
     // Insert it into media collection
     mediaCollection.insertOne(newMedia, function(err, result){
         assert.equal(err, null);
-        console.log("UPLOAD_MEDIA: Value of newId in new scope is: " + newId);
         console.log("Insertion of " + result.result.n + " photo with id " + result.ops[0]._id + " successful...");
         usersCollection.findOneAndUpdate({_id : newId},
             {$push : {posted_media : result.ops[0]._id}},
             {returnOriginal : false},
             function(err, result){
                 assert.equal(err, null);
-                console.log("Successfully updated user " + result.value.name + "'s posted_media number to " + result.value.posted_media.length);
                 callback();
             });
     });
@@ -391,13 +222,11 @@ function ownLike(db, newid, callback){
             assert.equal(err, null);
             // Get last post
             const lastPost = result.posted_media[result.posted_media.length - 1];
-            console.log("OWN_LIKE: Last post for user " + result._id + "had media_id " + lastPost);
             const newReaction = {
                 poster: newid,
                 recipient: result._id,
                 type: "heart"
             };
-            console.log("ADD_COMMENT: New reaction by poster: " + newReaction.poster + " to recipient " + newReaction.recipient);
 
             // Find and update the media in the media collection corresponding to this image
             mediaCollection.findOneAndUpdate({_id : lastPost},
@@ -437,7 +266,7 @@ function ownLike(db, newid, callback){
  * @param newId
  * @param callback
  */
-function lastTwo(db, newId, callback){
+function lastThree(db, newId, callback){
     const usersCollection = db.collection(usersColl);
     const mediaCollection = db.collection(mediaColl);
 
@@ -447,23 +276,20 @@ function lastTwo(db, newId, callback){
 
         // Get users you follow
         const followingList = result.following;
-        console.log("OWN_LIKE: User " + result._id + "follows " + followingList.length + " users.");
 
         // Find and update the media in the media collection corresponding to this image
         usersCollection.find({_id : { $in: followingList }}).toArray(function(err, result){
             assert.equal(err, null);
-            console.log("lastTwo: Returned " + result.length + " user docs...");
 
             // Combine all media into one array
             var i;
             var allMedia = [];
             for(i = 0; i < result.length; i++){
                 const curUser = result[i];
-                console.log("LAST_TWO: Number of posted media by user " + curUser._id + " is " + curUser.posted_media.length);
                 allMedia = allMedia.concat(curUser.posted_media);
             }
 
-            // Find top two media posted by following list
+            // Find top three media posted by following list
             mediaCollection.find({_id : {$in: allMedia}},
                 {limit : 3,
                  sort : [["time_posted", -1]]}).toArray(function(err, result){
@@ -499,7 +325,6 @@ function oneUser(db, newId, callback){
         // Find and update the media in the media collection corresponding to this image
         usersCollection.findOne({_id : otherUser}, function(err, result){
             assert.equal(err, null);
-            console.log("OTHER_USER: Number of posted media by user " + result._id + " is " + result.posted_media.length);
 
             // Find top two media posted by following list
             mediaCollection.find({_id : {$in: result.posted_media}},
@@ -526,7 +351,7 @@ function unfollow(db, newId, followingName, callback) {
         {returnOriginal : false},
         function(err, result){
             assert.equal(err, null);
-            console.log("UNFOLLOW: Does newId remain in followers list? " + result.value.followers.includes(newId));
+            console.log("Dan remains in Jacs followers list: " + result.value.followers.includes(newId));
 
             // Use unfollowed individual's id to update newId's following list
             usersCollection.findOneAndUpdate(
@@ -535,7 +360,7 @@ function unfollow(db, newId, followingName, callback) {
                 {returnOriginal : false},
                 function(err, result){
                     assert.equal(err, null);
-                    console.log("UNFOLLOW: Size of new user's followers list: " + result.value.following.length);
+                    console.log("Size of Dan's following list after unfollowing Jack: " + result.value.following.length);
                     callback();
                 });
         });
@@ -550,10 +375,10 @@ MongoClient.connect(url, function(err, client) {
     const followingName = "Jack Ricci";
 
     //Sign up for account
-    console.log("Adding user...");
+    console.log("\nSigning up for account...");
     addUser(db, function(newId){
         // Follow someone new
-        console.log("User " + newId + " attempting to follow someone new...");
+        console.log("\nUser " + newId + " attempting to follow someone new...");
         followJack(db, newId, followingName, function(){
 
             // Comment on a post
@@ -566,8 +391,8 @@ MongoClient.connect(url, function(err, client) {
                     console.log("\nLiking own photo by accident...");
                     ownLike(db, newId, function(){
                         // Find two most recent posts
-                        console.log("\nFinding two most recent posts in feed...");
-                        lastTwo(db, newId, function(){
+                        console.log("\nFinding three most recent posts in feed...");
+                        lastThree(db, newId, function(){
                             // See all of the photos from one other user
                             console.log("\nFinding all of Jack Riccis photos...");
                             oneUser(db, newId, function(){
